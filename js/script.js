8802f1cc3620a74ccc45604f7b35bd6e1200490c
Shameer Abdul Kader <shameerak896@gmail.com>
2023-11-21 21:36:12 +0530

    changed the display message
    changed the display message after submitting the form

--- js/script.js
+++ js/script.js
@@ -157,7 +157,7 @@ function sendMail(e) {
       document.getElementById("email").value = "";
       document.getElementById("place").value = "";
       console.log(res);
-      alert("Your message sent successfully!!");
+      alert("Your message sent successfully!!(We will contact you back via mail or via call)");
     })
     .catch((err) => console.log(err));
 }
